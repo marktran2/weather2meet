45f5d6949b808cc6cd8db43669d2c8bc764bc5ff
Mark Tran <z5421592@ad.unsw.edu.au>
2023-07-02 11:10:24 +1000

    Better date print

--- frontend/src/components/Forecast.jsx
+++ frontend/src/components/Forecast.jsx
@@ -3,7 +3,14 @@ import React from "react";
 
 const Forecast = (props) => {
 
-  const dataFC = props.data.map(item => {
+  const dateSubStr = props.date.substring(4, 15)
+
+  const date = new Date(dateSubStr)
+  const dateFormat = date.getFullYear() + "-" + (date.getMonth()+1).toString().padStart(2, "0") + "-" + date.getDate().toString().padStart(2, "0")
+
+  const dataFC = props.data.filter(
+    item => item.date.includes(dateFormat)
+  ).map(item => {
     return (
       <div>
         <span>{item.date} {item.condition}</span>
